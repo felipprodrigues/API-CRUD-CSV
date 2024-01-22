764f68b560c997c6ab48505df527f5572115d326
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2024-01-22 18:43:00 -0300

    feat: add middleware

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { Json } from './middleware/json.js'
 
 // POST - /tasks
 // GET - /tasks
@@ -11,21 +12,23 @@ const tasks = []
 const server = http.createServer(async(req, res) => {
   const {method, url} = req
 
+  await Json(req, res)
 
-  if(method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(tasks))
+  if(method === 'GET' && url === '/tasks') {
+    return res.end(JSON.stringify(tasks))
   }
 
-  if(method === 'POST' && url === '/users') {
+  if(method === 'POST' && url === '/tasks') {
+
+    const {title, descrpition, completed_at, created_at, updated_at} = req.body
+
     tasks.push({
       id: 1,
-      title: 'comprar pão',
-      descrpition: 'passar na padoca e comprar pão',
-      completed_at: null,
+      title,
+      descrpition,
+      completed_at,
       created_at: '19/01/2024',
-      updated_at: null
+      updated_at
     })
 
     return res.end('Criação de tasks')
